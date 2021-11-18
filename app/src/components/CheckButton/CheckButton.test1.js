f4bdee13afe02e4f9d5a60cdb82aefd895f78a23
RyuJoonSik <71337000+RyuJoonSik@users.noreply.github.com>
2021-11-18 14:48:17 +0900

    react 스크래퍼

--- app/src/components/CheckButton/CheckButton.test1.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import CheckButton from './CheckButton';
-
-describe('<CheckButton />', () => {
-  it('has all check button', () => {
-    render(<CheckButton />);
-    screen.getByText('All check');
-  });
-});
--- app/src/components/ExcelButton/ExcelButton.test1.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import ExcelButton from './ExcelButton';
-
-describe('<ExcelButton />', () => {
-  it('has all check button', () => {
-    render(<ExcelButton />);
-    screen.getByText('Excel export');
-  });
-});
